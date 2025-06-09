c3336fa1fb370e020f0a9d3ec07051ec1a94c42e
Yeomong412 <yeomong0804@sasa.hs.kr>
2025-06-10 08:57:03 +0900

    코드: 기본 구조

---
+++ main.py
@@ -0,0 +1,11 @@
+class task():
+    def __init__(self):
+        pass
+
+class Heater_RTG(task):
+    def __init__(self):
+        pass
+
+class Rendezvous(task):
+    def __init__(self):
+        pass
