33390303ace6fd4b6558c18e6b0898564225ef24
SuminHong2008 <sasain@sasa.hs.kr>
2025-06-12 16:00:17 +0900

    new branch committed

--- main.py
+++ main.py
@@ -61,4 +61,45 @@ class Rendezvous(task):
     def __init__(self):
         pass #
 
+import time
+import random
+class sandstorm():
+    def __init__(self,mode):
+        if(mode=="easy"):
+            self.stormcome(10)
+        if(mode=="intermediate"):
+            self.stormcome(30)
+        if(mode=="hard"):
+            self.stormcome(50)
+        
+    def stormcome(self, x):
+        l=[[0 for i in range(19)] for j in range(19)]
+        a,b=map(int,input("Choose your Base Coordinates").split())
+        l[a][b]='H'
+        for k in range(100):
+            num1 = random.randint(0, 18)
+            num2 = random.randint(0, 18)
+            num3 = random.randint(0,1)
+            if(num3):
+                for i in range(0,19):
+                    l[num1][i]=1
+            else:
+                for i in range(0,19):
+                    l[i][num2]=1
+            move=input()        
+            time.sleep(0.7)
+            for i in range(19):
+                for j in range(19):
+                    print(l[i][j],end=" ")
+                print()
+            for i in range(19):
+                for j in range(19):
+                    if(l[i][j]==1):
+                        l[i][j]='S'
+                        if(l[a][b]=='S'):
+                            print("Oh no The Base Has Been Destroyed")
+                            break
+                    elif(l[i][j]=='S'):
+                        l[i][j]=0
+
 Heater_RTG()
